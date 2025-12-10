2e6e89a728f9f87e9a88ab1c904188e56747d3db
papercrane1001 <wallison@westmont.edu>
2025-12-09 17:03:27 -0800

    I knew I'd have to get into the C# with this one.  I knew there was no way it would resolve in the xml.

---
+++ Source/MechBlinkUpgrade/UpgradeComp_AddHediffLite.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using MU;
+using UnityEngine;
+using Verse;
+
+namespace MechBlinkUpgrade
+{
+    public class UpgradeComp_AddHediffLite : UpgradeComp
+    {
+        public bool active = true;
+        [Unsaved(false)]
+        private Texture2D activateTex;
+        public UpgradeComp_AddHediffLite Props
+        {
+            get
+            {
+                return (UpgradeComp_AddHediffLite)this.props;
+            }
+        }
+
+        public override void PostExposeData()
+		{
+			base.PostExposeData();
+			Scribe_Values.Look<bool>(ref this.active, "active", true, false);
+		}
+    }
+}
